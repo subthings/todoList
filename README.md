7c32622a732d09f3fe8c13f496f0e9fd3f855e95
AnnaZaychikova <AnnaZaychikova@noveogroup.com>
2020-09-28 10:16:53 +0700

    Added mongoose

---
+++ README.md
@@ -0,0 +1,3 @@
+# todolist-v1
+
+Todolist built using node js and mongoose
--- app.js
+++ app.js
@@ -1,31 +1,54 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const date = require(__dirname + "/date.js");
+const mongoose = require("mongoose");
+
+const dbName = "todoListDB";
+const mongoUrl = "mongodb://localhost/" + dbName;
 
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"))
 app.set("view engine", "ejs")
 
-const todoList = [];
+mongoose.connect(mongoUrl, {useNewUrlParser: true, useUnifiedTopology: true});
+
+const todoItemSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Please add your todo"]
+    }
+});
+
+const TodoItem = mongoose.model("TodoItem", todoItemSchema);
 
 app.get("/", (req, res) => {
-    
-    res.render("list", {weekDay: date.getDate(), todoList: todoList});
+    TodoItem.find((err, list) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("list", {weekDay: date.getDate(), todoList: list});
+        }
+    })
 })
 
 app.post("/", (req, res) => {
-    todoList.push(req.body.todoItem);
+    const todoItem = new TodoItem({
+        name: req.body.todoItem
+    })
+    todoItem.save()
     res.redirect("/");
 })
 
-function isWeekend(day) {
-    if (day in [6, 0]) {
-        return "Weekend";   
-    }
-
-    else return "Workday";
-}
+app.post("/delete", (req, res) => {
+    const id = req.body.deleteItem;
+    TodoItem.findByIdAndRemove(id, (err) => {
+        if (err) {
+            console.log(err);
+        }
+    });
+    res.redirect("/");
+})
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
